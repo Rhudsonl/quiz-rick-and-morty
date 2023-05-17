8aa76a7cfb506dfedfc62f5d42d4b4b243793aa8
RenneRhudson <105531444+Rhudsonl@users.noreply.github.com>
2023-05-17 20:09:15 -0300

    Add files via upload
    Description
    The code consists of a quiz about the animated series "Rick and Morty". The quiz is implemented using HTML, CSS, and JavaScript. JavaScript is responsible for calculating the user's score based on the provided answers and displaying the result.
    
    Features
    Answer Selection: Users can select an answer option for each question in the quiz.
    Score Calculation: The code calculates the user's score based on the correct answers provided.
    Result Display: The quiz result is shown to the user, indicating the percentage of correct answers.
    Prerequisites
    Before running the code, make sure you have a compatible web browser installed on your device.
    
    How to Run
    Follow the steps below to run the quiz code:
    
    Download the HTML, CSS, and JavaScript files of the quiz.
    Open the HTML file (index.html) in a compatible web browser.
    Usage Example
    When running the quiz code, you will be presented with a series of questions about Rick and Morty. Select the correct option for each question and click the "Submit" button. The result will be displayed, showing the percentage of correct answers.

---
+++ app.js
@@ -0,0 +1,46 @@
+const form = document.querySelector('form')
+const result = document.querySelector('.result')
+const percentageResult = result.querySelector('span')
+const correctAnswers = ['B', 'C', 'A', 'A', 'A']
+
+let score = 0 
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+  const  userAnsewrs = [
+    event.target.flexRadioDefault.value,
+    event.target.flexRadioDefault2.value,
+    event.target.flexRadioDefault3.value,
+    event.target.flexRadioDefault4.value,
+    event.target.flexRadioDefault5.value]
+
+    userAnsewrs.forEach((answers, index) => {
+        if (answers === correctAnswers[index]) {
+            score += 20
+        }
+    })
+    
+    result.classList.remove('d-none')
+    scrollTo({
+        top: 0,
+        left: 0
+    })
+
+     let counter = 0
+     
+     const time = setInterval(() => {
+         percentageResult.textContent = `${counter}%`
+         
+         
+         if(counter === score) {
+             clearInterval(time)
+            }
+            counter++
+        },30)
+        
+           
+        
+    
+})
+
+
